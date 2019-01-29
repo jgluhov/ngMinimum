222942024b880103cb919d5dd37e939393af8443
Evgeny Glukhov <evglukhov@luxoft.com>
2019-01-29 13:11:57 +0300

    minimum project configuration for angular

--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log('1234');
+import 'core-js';
+import 'zone.js';
+import { Component, NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  template: 'Hello World!'
+})
+class AppComponent {
+}
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
+})
+class AppModule {
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
